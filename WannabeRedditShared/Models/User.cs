08252d9d630a09f62170758affa286087ad06c5e
juli175h <jbramsen1212@gmail.com>
2023-10-03 11:12:31 +0200

    Update User.cs

--- WannabeRedditShared/Models/User.cs
+++ WannabeRedditShared/Models/User.cs
@@ -3,11 +3,13 @@
 public class User
 {
     public string Name { get; set; }
+    public string PassWord { get; set; }
 
     // TODO(rune): Hvad ellers?
 
-    public User(string name)
+    public User(string name, string passWord)
     {
         Name = name;
+        PassWord = passWord;
     }
 }
