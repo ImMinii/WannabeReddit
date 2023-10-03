8b077712c7a2b413fe836026597ddfac5d5b0307
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-10-03 11:30:48 +0200

    DTO

--- WannabeRedditShared/Domain/DTOs/PostCreate.cs
+++ WannabeRedditShared/Domain/DTOs/PostCreate.cs
@@ -1,6 +1,17 @@
-﻿namespace WannabeRedditShared.Domain.DTOs;
+﻿using WannabeRedditShared.Domain.Models;
+
+namespace WannabeRedditShared.Domain.DTOs;
 
 public class PostCreate
 {
+    public User Author { get; set; } // TODO(rune): Måske burde Author være implicit, alt efter om man er logget ind.
+    public string Title { get; set; }
+    public string Body { get; set; }
 
+    public PostCreate(User author, string title, string body)
+    {
+        Author = author;
+        Title = title;
+        Body = body;
+    }
 }
--- WannabeRedditShared/Domain/DTOs/PostUpdate.cs
+++ WannabeRedditShared/Domain/DTOs/PostUpdate.cs
@@ -2,5 +2,14 @@
 
 public class PostUpdate
 {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
 
+    public PostUpdate(int id, string title, string body)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+    }
 }
