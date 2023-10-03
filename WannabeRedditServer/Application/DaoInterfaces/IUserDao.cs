03a5239da2d327f598119db592caa6a8d657e2f1
juli175h <jbramsen1212@gmail.com>
2023-10-03 11:44:28 +0200

    IUserDao

--- WannabeRedditServer/Application/DaoInterfaces/IUserDao.cs
+++ WannabeRedditServer/Application/DaoInterfaces/IUserDao.cs
@@ -1,6 +1,12 @@
-﻿namespace WannabeRedditServer.Application.DaoInterfaces;
+﻿using WannabeRedditShared.Domain.DTOs;
+using WannabeRedditShared.Domain.Models;
+
+namespace WannabeRedditServer.Application.DaoInterfaces;
 
 public interface IUserDao
 {
-    
+    Task<User> CreateAsync(User user);
+    Task<User?> GetByUsernameAsync(string userName);
+    Task<IEnumerable<User>> GetAsync(SearchUserParametersDto searchParameters);
+    Task<User?> GetByIdAsync(int id);
 }
