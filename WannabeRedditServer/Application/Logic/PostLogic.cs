6bc70b4d4c342481eb04c08c5b94be0925edd095
juli175h <jbramsen1212@gmail.com>
2023-10-24 09:15:01 +0200

    Post Logic - so far so good

--- WannabeRedditServer/Application/Logic/PostLogic.cs
+++ WannabeRedditServer/Application/Logic/PostLogic.cs
@@ -1,6 +1,60 @@
-﻿namespace WannabeRedditServer.Application.Logic;
+﻿using WannabeRedditServer.Application.DaoInterfaces;
+using WannabeRedditServer.Application.LogicInterfaces;
+using WannabeRedditShared.Domain.DTOs;
+using WannabeRedditShared.Domain.Models;
 
-public class PostLogic
+namespace WannabeRedditServer.Application.Logic;
+
+public class PostLogic : IPostLogic
 {
+    private readonly IPostDao PostDao;
+    private readonly IUserDao UserDao;
+
+    public PostLogic(IPostDao postDao, IUserDao userDao)
+    {
+        PostDao = postDao;
+        UserDao = userDao;
+    }
+
+
+    public async Task<Post> CreateAsync(PostCreate dto)
+    {
+        User? user = await UserDao.GetByIdAsync(dto.Author.Id);
+        if ( user == null)
+        {
+            throw new Exception($"Author with id {dto.Author.Id} was not found");
+        }
+
+        Post post = new Post(dto.Title, dto.Body, user);
+
+        ValidatePost(post);
+        Post created = await PostDao.CreateAsync(post);
+        return created;
+    }
     
+
+    public Task<IEnumerable<Post>> GetAsync(PostSearch dto)
+    {
+        return PostDao.GetAsync(dto);
+    }
+
+    public Task UpdateAsync(PostUpdate dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PostBasic> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void ValidatePost(Post post)
+    {
+        throw new NotImplementedException();
+    }
 }
--- WannabeRedditServer/Application/LogicInterfaces/IPostLogic.cs
+++ WannabeRedditServer/Application/LogicInterfaces/IPostLogic.cs
@@ -1,6 +1,13 @@
-﻿namespace WannabeRedditServer.Application.LogicInterfaces;
+﻿using WannabeRedditShared.Domain.DTOs;
+using WannabeRedditShared.Domain.Models;
+
+namespace WannabeRedditServer.Application.LogicInterfaces;
 
 public interface IPostLogic
 {
-    
+    Task<Post> CreateAsync(PostCreate dto);
+    Task<IEnumerable<Post>> GetAsync(PostSearch dto);
+    Task UpdateAsync(PostUpdate dto);
+    Task DeleteAsync(int id);
+    Task<PostBasic> GetByIdAsync(int id);
 }
