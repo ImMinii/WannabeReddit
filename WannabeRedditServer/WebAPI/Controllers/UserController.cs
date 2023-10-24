e6c177e5f00d0529c2029c7eb8d4491c3aa92b15
juli175h <jbramsen1212@gmail.com>
2023-10-24 10:45:06 +0200

    UserController

---
+++ WannabeRedditServer/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WannabeRedditServer.Application.LogicInterfaces;
+using WannabeRedditShared.Domain.DTOs;
+using WannabeRedditShared.Domain.Models;
+
+namespace WannabeRedditServer.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserLogic userLogic;
+
+    public UserController(IUserLogic userLogic)
+    {
+        this.userLogic = userLogic;
+    }
+    
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateAsync(UserCreate dto)
+    {
+        try
+        {
+            User user = await userLogic.CreateAsync(dto);
+            return Created($"/users/{user.Id}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetAsync([FromQuery] string? userNameContains)
+    {
+        try
+        {
+            SearchUserParametersDto parameters = new(userNameContains);
+            IEnumerable<User> users = await userLogic.GetAsync(parameters);
+            return Ok(users);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
